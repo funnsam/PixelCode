6e5e9b4bec2de76d350d7d596a4a01406a8eb1ab
funnsam <funnsam8@gmail.com>
2024-04-07 08:48:46 +0000

    some stuff

---
+++ src/build_without_venv.sh
@@ -0,0 +1,14 @@
+# Build UFOs from images and fea files.
+python3 build_from_images.py
+
+# Variable font (experimental)
+# fontmake ../build/PixelCode.designspace -o variable --output-dir ../dist/variable
+# woff2_compress ../dist/variable/*.ttf
+
+# Call fontmake to build the UFOs in to OTF and TTF fonts.
+fontmake -u ../build/*.ufo -o otf --output-dir ../dist/otf
+fontmake -u ../build/*.ufo -o ttf --output-dir ../dist/ttf
+
+# Create woff2 fonts from the TTFs.
+ls ../dist/ttf/*.ttf | xargs -I {} woff2_compress {}
+mv ../dist/ttf/*.woff2 ../dist/woff2
