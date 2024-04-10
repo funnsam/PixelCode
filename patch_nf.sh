aca7375238bf15c3e7844c0f6b451ccc3b46c450
funnsam <funnsam8@gmail.com>
2024-04-10 17:42:01 +0800

    update

--- patch_nf.sh
+++ patch_nf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git clone git@github.com:ryanoasis/nerd-fonts.git --depth 1
 cd nerd-fonts
-./font-patcher ../dist/variable/PixelCode-VF.ttf
+./font-patcher ../dist/ttf/PixelCode.ttf
+mv PixelCodeNerdFont-Regular.ttf .. -f
